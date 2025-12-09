f30809f5d1e978395d82171ff3cd1fdbfaf63b9f
ilariamotta <ilaria.motta93@gmail.com>
2025-12-09 15:28:25 +0100

    feat: creazione navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export default function Header() {
+const links = [
+    {
+        pagina: "Homepage",
+        path: "/",
+    },
+    {
+        pagina: "Chi Siamo",
+        path: "/about",
+    },
+    {
+        pagina: "Prodotti",
+        path: "/prodotti",
+    },
+];
+
+    return (
+        <section>
+            <div className="container d-flex justify-content-between py-2 align-items-center bg-white">
+                <Link to="/"><img src="\src\assets\logoholder.png" alt="logo" /></Link>
+                    <ul className="d-flex justify-content-between gap-5">
+                        {links.map((link, index) => {
+                            return(
+                                <NavLink key={index} to={link.path}>{link.pagina}</NavLink>
+                            )
+                        })}
+                    </ul>
+                
+            </div>
+        </section>
+
+    )
+}
--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function DefaultLayout() {
 return (
 <>
-<header>sono header</header>
+<Header />
 <main><Outlet/></main>
 <footer>sono footer</footer>
 </>
